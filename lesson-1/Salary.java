137203f23f5595d51f7b30ac17647d57fd386360
Albert Ramos <albertramos.dev@gmail.com>
2022-03-24 14:02:11 -0700

    Update to generalize program input

--- lesson-1/Salary.java
+++ lesson-1/Salary.java
@@ -2,14 +2,32 @@ import java.util.Scanner;
 
 public class Salary {                         
    public static void main (String [] args) { 
-      int wage;                          
+      double hourlyWage;
+      double annualSalary;
+      double monthlySalary;
+      double hoursPerWeek;
+      int weeksPerYear;                          
 
       Scanner scnr = new Scanner(System.in);
-      wage = scnr.nextInt();
+      System.out.print("Hourly wage is: $");
+      hourlyWage = scnr.nextDouble();
 
-      System.out.print("Salary is ");         
-      System.out.println(wage * 40 * 52);
+      System.out.print("Work hours per week: ");
+      hoursPerWeek = scnr.nextDouble();
+
+      System.out.print("Work weeks paid per year: ");
+      weeksPerYear = scnr.nextInt();
+
+      annualSalary = hourlyWage * hoursPerWeek * weeksPerYear;
+      System.out.print("Annual salary is: $");         
+      System.out.printf("%.2f", annualSalary);
+      System.out.println();
       
+      monthlySalary = (hourlyWage * hoursPerWeek * weeksPerYear) / 12;
+      System.out.print("Monthly salary is: $");
+      System.out.printf("%.2f", monthlySalary);
+      System.out.println();
+
       scnr.close();
    } 
 }
