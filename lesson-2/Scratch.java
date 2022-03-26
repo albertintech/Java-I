93dbcafc31c901c6a25b6ace389ab541f7c7609c
Albert Ramos <albertramos.dev@gmail.com>
2022-03-26 11:27:05 -0700

    Add scratch file

---
+++ lesson-2/Scratch.java
@@ -0,0 +1,10 @@
+public class Scratch {
+    public static void main(String[] args) {
+        // int y = 9;
+        // int z = 3;
+        // double x;
+        // x = (y / (z + 1.0));
+
+        System.out.println(3 + 1.0);
+    }
+}
