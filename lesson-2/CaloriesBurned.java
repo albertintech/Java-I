94c3fe54ffc178c4fee2e50f3689d9a690ac8b5e
Albert Ramos <albertramos.dev@gmail.com>
2022-03-26 12:54:08 -0700

    Completed solution

--- lesson-2/CaloriesBurned.java
+++ lesson-2/CaloriesBurned.java
@@ -8,16 +8,18 @@ public class CaloriesBurned {
         int weight;
         int heartRate;
         int time;
+        double calories;
 
         age = scnr.nextInt();
         weight = scnr.nextInt();
         heartRate = scnr.nextInt();
         time = scnr.nextInt();
+        calories = ((age * 0.2757) + (weight * 0.03295) +
+        (heartRate * 1.0781) - 75.4991) * (time/8.368);
 
-        System.out.println(age);
-        System.out.println(weight);
-        System.out.println(heartRate);
-        System.out.println(time);
+        System.out.print("Calories: ");
+        System.out.printf("%.2f", calories);
+        System.out.println(" calories");
 
         scnr.close();
     }
