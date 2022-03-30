483e88012efb7afadcb6efb5735318c9a71a1ed8
Albert Ramos <albertramos.dev@gmail.com>
2022-03-30 10:42:19 -0700

    Add code stub

--- lesson-3/ExactChange.java
+++ lesson-3/ExactChange.java
@@ -6,7 +6,27 @@ import java.util.Scanner;
 
 public class ExactChange {
     public static void main(String[] args) {
+        int quarters;
+        int dimes;
+        int nickels;
+        int pennies;
+        double dollars;
+
+        final double Q_VALUE = 0.25;
+        final double D_VALUE = 0.10;
+        final double N_VALUE = 0.05;
+        final double P_VALUE = 0.01;
+
         Scanner scnr = new Scanner(System.in);
+        pennies = scnr.nextInt();
+        quarters = scnr.nextInt();
+        dimes = scnr.nextInt();
+        nickels = scnr.nextInt();
+
+        dollars = (quarters * Q_VALUE) + (dimes * D_VALUE) +
+                (nickels * N_VALUE) + (pennies * P_VALUE);
+
+        System.out.printf("Amount: $%.2f\n", dollars);
 
         scnr.close();
     }
