02698ce2da2f9b76ab54c07b53c7893973c6cf8a
Albert Ramos <albertramos.dev@gmail.com>
2022-03-30 06:48:44 -0700

    Add file

---
+++ lesson-3/CostStructure.java
@@ -0,0 +1,34 @@
+/*
+An airline describes airfare as follows. A normal ticket's base cost is $300. Persons aged 60 or over have a base cost of $290. Children 2 or under have $0 base cost. A carry-on bag costs $10. A first checked bag is free, second is $25, and each additional is $50. Given inputs of age, carry-on (0 or 1), and checked bags (0 or greater), compute the total airfare.
+
+Hints:
+
+First use an if-else statements to assign airFare with the base cost
+
+Use another if statement to update airFare for a carryOn
+
+Finally, use another if-else statement to update airFare for checked bags
+
+Think carefully about what expression correctly calculates checked bag cost when bags are 3 or more
+*/
+
+import java.util.Scanner;
+
+public class CostStructure {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        // int passengerAge;
+        // int carryOns;
+        // int checkedBags;
+        // int airFare;
+
+        // passengerAge = scnr.nextInt();
+        // carryOns = scnr.nextInt();
+        // checkedBags = scnr.nextInt();
+
+        // /* Type your code here. */
+
+        // System.out.println(airFare);
+        scnr.close();
+    }
+}
