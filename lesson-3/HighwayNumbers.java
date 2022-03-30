bfd00a4507e87f338e06ff6931d8b32e877de3ef
Albert Ramos <albertramos.dev@gmail.com>
2022-03-30 06:50:57 -0700

    Update file

--- lesson-3/HighwayNumbers.java
+++ lesson-3/HighwayNumbers.java
@@ -1,5 +1,7 @@
 /*
-Write a program whose inputs are three integers, and whose output is the smallest of the three values.
+Primary U.S. interstate highways are numbered 1-99. Odd numbers (like the 5 or 95) go north/south, and evens (like the 10 or 90) go east/west. Auxiliary highways are numbered 100-999, and service the primary highway indicated by the rightmost two digits. Thus, I-405 services I-5, and I-290 services I-90. Note: 200 is not a valid auxiliary highway because 00 is not a valid primary highway number.
+
+Given a highway number, indicate whether it is a primary or auxiliary highway. If auxiliary, indicate what primary highway it serves. Also indicate if the (primary) highway runs north/south or east/west.
 */
 
 import java.util.Scanner;
@@ -7,10 +9,10 @@ import java.util.Scanner;
 public class HighwayNumbers {
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
-        int highwayNumber;
-        int primaryNumber;
+        // int highwayNumber;
+        // int primaryNumber;
 
-        highwayNumber = scnr.nextInt();
+        // highwayNumber = scnr.nextInt();
 
         scnr.close();
     }
