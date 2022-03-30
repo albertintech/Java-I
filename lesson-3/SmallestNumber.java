001b7b5afcd45781ac3581e9bf4f8bd5dd401184
Albert Ramos <albertramos.dev@gmail.com>
2022-03-30 06:30:54 -0700

    Add new program file

---
+++ lesson-3/SmallestNumber.java
@@ -0,0 +1,13 @@
+/*
+Write a program whose inputs are three integers, and whose output is the smallest of the three values.
+*/
+
+import java.util.Scanner;
+
+public class SmallestNumber {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        
+        scnr.close();
+    }
+}
