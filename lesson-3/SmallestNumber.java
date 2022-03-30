963dc7d64f53ee1b958a5b1e3cf0ec2efa23d430
Albert Ramos <albertramos.dev@gmail.com>
2022-03-30 07:06:52 -0700

    Add solution

--- lesson-3/SmallestNumber.java
+++ lesson-3/SmallestNumber.java
@@ -7,7 +7,22 @@ import java.util.Scanner;
 public class SmallestNumber {
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
-        
+        int num1;
+        int num2;
+        int num3;
+
+        num1 = scnr.nextInt();
+        num2 = scnr.nextInt();
+        num3 = scnr.nextInt();
+
+        if ((num1 < num2) && (num1 < num3)) {
+            System.out.println(num1);
+        } else if ((num2 < num3) && (num2 < num1)) {
+            System.out.println(num2);
+        } else {
+            System.out.println(num3);
+        }
+            
         scnr.close();
     }
 }
