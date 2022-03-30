9482c3ca3c834ba1ab51a2bf5892ab97540b498f
Albert Ramos <albertramos.dev@gmail.com>
2022-03-30 07:19:58 -0700

    Add solution

--- lesson-3/HighwayNumbers.java
+++ lesson-3/HighwayNumbers.java
@@ -9,11 +9,23 @@ import java.util.Scanner;
 public class HighwayNumbers {
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
-        // int highwayNumber;
-        // int primaryNumber;
+        int highwayNumber;
+        int primaryNumber;
 
-        // highwayNumber = scnr.nextInt();
-
+        highwayNumber = scnr.nextInt();
+        if (highwayNumber > 99 && highwayNumber <= 999 && highwayNumber != 200) {
+            primaryNumber = highwayNumber % 100;
+            System.out.println("I-" + highwayNumber + " is auxiliary, serving I-" + primaryNumber + ", "
+                    + (primaryNumber % 2 == 0 ? "going east/west." : "going north/south."));
+        } else if (highwayNumber > 0 && highwayNumber < 100) {
+            if (highwayNumber % 2 == 0) {
+                System.out.println("I-" + highwayNumber + " is primary, going east/west.");
+            } else {
+                System.out.println("I-" + highwayNumber + " is primary, going north/south.");
+            }
+        } else {
+            System.out.println(highwayNumber + " is not a valid interstate highway number.");
+        }
         scnr.close();
     }
 }
