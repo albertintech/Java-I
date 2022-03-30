cdb416a535f73013decaea53ba969e2dc0d816af
Albert Ramos <albertramos.dev@gmail.com>
2022-03-30 06:50:05 -0700

    Add file

---
+++ lesson-3/LoginName.java
@@ -0,0 +1,13 @@
+/*
+Write a program that creates a login name for a user, given the user's first name, last name, and a four-digit integer as input. Output the login name, which is made up of the first five letters of the last name, followed by the first letter of the first name, and then the last two digits of the number (use the % operator). If the last name has less than five letters, then use all letters of the last name.
+*/
+
+import java.util.Scanner;
+
+public class LoginName {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+
+        scnr.close();
+    }
+}
