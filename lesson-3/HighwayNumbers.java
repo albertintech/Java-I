ddbb2e3e29fc50c45c6ccc5e0ac7287699935eeb
Albert Ramos <albertramos.dev@gmail.com>
2022-03-30 06:32:25 -0700

    Add new program file

---
+++ lesson-3/HighwayNumbers.java
@@ -0,0 +1,17 @@
+/*
+Write a program whose inputs are three integers, and whose output is the smallest of the three values.
+*/
+
+import java.util.Scanner;
+
+public class HighwayNumbers {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int highwayNumber;
+        int primaryNumber;
+
+        highwayNumber = scnr.nextInt();
+
+        scnr.close();
+    }
+}
