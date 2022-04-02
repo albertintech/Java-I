df94778baabcbbe3fca4759efd5bdaed1de11796
Albert Ramos <albertramos.dev@gmail.com>
2022-04-02 16:28:41 -0700

    Add remove double spaces program

---
+++ lesson-4/SingleSpaces.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class SingleSpaces {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        String userText;
+
+        System.out.print("Enter sentence: ");
+        userText = scnr.nextLine();
+        scnr.close();
+        
+        while (userText.indexOf("  ") != -1) {
+            // At least one double-space still exists
+            // Replace all double-spaces by one space
+            userText = userText.replace("  ", " ");
+        }
+
+        System.out.print("New sentence: " + userText);
+    }
+}
