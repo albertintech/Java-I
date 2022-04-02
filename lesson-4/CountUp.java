de87e9aa5b01fde71d35a860a31bba3e6de261f9
Albert Ramos <albertramos.dev@gmail.com>
2022-04-02 10:35:41 -0700

    Add while loop demo program

---
+++ lesson-4/CountUp.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class CountUp {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+
+        int currPower;
+        char userChar;
+
+        currPower = 2;
+        userChar = 'y';
+
+        while (userChar == 'y') {
+            System.out.println(currPower);
+            currPower *= 2;
+            System.out.print("Enter 'y' to continue, any other character to stop: ");
+            userChar = scnr.next().charAt(0);
+        }
+
+        System.out.println("Done");
+        scnr.close();
+    }
+}
