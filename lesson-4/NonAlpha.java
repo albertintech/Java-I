d0787fa74cdb3585b1a30a720576e8b4c1af5d6e
Albert Ramos <albertramos.dev@gmail.com>
2022-04-05 06:55:08 -0700

    Add solution

--- lesson-4/NonAlpha.java
+++ lesson-4/NonAlpha.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
 public class NonAlpha {
-    
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        String sentence = scnr.nextLine();
+        String result = "";
+        for (int i = 0; i < sentence.length(); i++) {
+            if (Character.isAlphabetic(sentence.charAt(i)))
+                result += sentence.charAt(i);
+        }
+        System.out.println(result);
+        scnr.close();
+    }
 }
