d57eb61fd7ee68cdb450da51ec29bde6b549161c
Albert Ramos <albertramos.dev@gmail.com>
2022-04-05 07:09:16 -0700

    Add solution

--- lesson-4/ReverseString.java
+++ lesson-4/ReverseString.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
+
 public class ReverseString {
-    
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        String s = scnr.nextLine();
+        while (!(s.equals("Done") || s.equals("done") || s.equals("d"))) {
+            for (int i = s.length() - 1; i >= 0; i--) {
+                System.out.print(s.charAt(i));
+            }
+            System.out.println();
+            s = scnr.nextLine();
+        }
+        scnr.close();
+    }
 }
