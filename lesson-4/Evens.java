31a0e54ecdb5cb7144eff57133a8a147645f7169
Albert Ramos <albertramos.dev@gmail.com>
2022-04-05 07:17:08 -0700

    Add solution

--- lesson-4/Evens.java
+++ lesson-4/Evens.java
@@ -1,3 +1,28 @@
+import java.util.Scanner;
+
 public class Evens {
-    
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int n;
+        int i;
+
+        System.out.println("Enter an integer:");
+        n = scnr.nextInt();
+        scnr.close();
+
+        System.out.print("Sequence: ");
+
+        if (n < 0) {
+            n = 0;
+        }
+
+        if ((n % 2) == 1) {
+            n = n - 1;
+        }
+
+        for (i = n; i >= 0; i = i - 2) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
 }
