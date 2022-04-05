f6f8bd4c2a6991fe79b0f8dcdbc44eef87fa4414
Albert Ramos <albertramos.dev@gmail.com>
2022-04-05 07:19:34 -0700

    Add solution

--- lesson-4/Rocket.java
+++ lesson-4/Rocket.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+
 public class Rocket {
-    
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int rocketHeight = 0;
+        int initialVelocity = 0;
+        int timeSinceLaunch = 0;
+
+        initialVelocity = scnr.nextInt();
+
+        while (rocketHeight >= 0) {
+            System.out.println(timeSinceLaunch + " " + rocketHeight);
+            timeSinceLaunch = timeSinceLaunch + 1;
+            rocketHeight = (initialVelocity * timeSinceLaunch) - (5 * timeSinceLaunch * timeSinceLaunch);
+        }
+        scnr.close();
+    }
 }
