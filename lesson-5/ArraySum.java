e446c49d4284c681434595ea0a8be7aceeeaf2e5
Albert Ramos <albertramos.dev@gmail.com>
2022-04-06 15:04:14 -0700

    Add do while loop with scanner close

--- lesson-5/ArraySum.java
+++ lesson-5/ArraySum.java
@@ -3,24 +3,29 @@ import java.util.Scanner;
 public class ArraySum {
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
-        final int NUM_ELEMENTS = 8;
-        int[] userVals = new int[NUM_ELEMENTS];
-        int i;
-        int sumVal = 0;
+        int test = 0;
 
-        // Prompt user to populate array
-        System.out.println("Enter " + NUM_ELEMENTS + " integer values: ");
-        for (i = 0; i < userVals.length; ++i) {
-            userVals[i] = scnr.nextInt();
-            System.out.println("Value added to array: " + userVals[i]);
-        }
+        do {
+            final int NUM_ELEMENTS = 8;
+            int[] userVals = new int[NUM_ELEMENTS];
+            int i;
+            int sumVal = 0;
 
-        // Determine sum of array
-        for (i = 0; i < userVals.length; ++i) {
-            sumVal = sumVal + userVals[i];
-        }
+            // Prompt user to populate array
+            System.out.println("Enter " + NUM_ELEMENTS + " integer values: ");
+            for (i = 0; i < userVals.length; ++i) {
+                userVals[i] = scnr.nextInt();
+                System.out.println("Value added to array: " + userVals[i]);
+            }
 
-        System.out.println("Sum of array integers: " + sumVal);
+            // Determine sum of array
+            for (i = 0; i < userVals.length; ++i) {
+                sumVal = sumVal + userVals[i];
+            }
+
+            System.out.println("Sum of array integers: " + sumVal);
+        } while (test != 0);
         scnr.close();
     }
+
 }
