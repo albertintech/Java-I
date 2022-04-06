97fc5336fe7fe142db00f9ed3838291dc3ec3da3
Albert Ramos <albertramos.dev@gmail.com>
2022-04-06 07:39:58 -0700

    Update file with additional array

--- lesson-5/ArrayExample.java
+++ lesson-5/ArrayExample.java
@@ -1,11 +1,17 @@
 public class ArrayExample {
     public static void main(String[] args) {
         int[] itemCounts = new int[3];
+        int[] gameScores;
+        gameScores = new int[5];
 
         itemCounts[0] = 122;
         itemCounts[1] = 119;
         itemCounts[2] = 117;
 
+        gameScores[4] = 99;
+
         System.out.println(itemCounts[1]);
+        System.out.println(gameScores[0]);
+        System.out.println(gameScores[4]);
     }
 }
