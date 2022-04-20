379949ba36e43002d1485a2d24f885df3c2c51ec
Albert Ramos <albertramos.dev@gmail.com>
2022-04-20 14:31:30 -0700

    Add triangle program

---
+++ lesson-9/TriangleArea.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class TriangleArea {
+    @SuppressWarnings("resource")
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+
+        Triangle triangle1 = new Triangle();
+        Triangle triangle2 = new Triangle();
+
+        double base1 = scnr.nextDouble();
+        double height1 = scnr.nextDouble();
+        double base2 = scnr.nextDouble();
+        double height2 = scnr.nextDouble();
+
+        triangle1.setBase(base1);
+        triangle1.setHeight(height1);
+
+        triangle2.setBase(base2);
+        triangle2.setHeight(height2);
+
+        System.out.println("Triangle with smaller area:");
+
+        if (triangle1.getArea() < triangle2.getArea()) {
+            triangle1.printInfo();
+        } else {
+            triangle2.printInfo();
+        }
+    }
+}
