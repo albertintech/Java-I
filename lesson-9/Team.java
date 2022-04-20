0d5be6d582965af502ad26a6b6d60f43ab176345
Albert Ramos <albertramos.dev@gmail.com>
2022-04-20 15:47:14 -0700

    Add team winning or losing program

--- lesson-9/Team.java
+++ lesson-9/Team.java
@@ -1,3 +1,52 @@
 public class Team {
-    
+    // TODO: Declare private fields - name, wins, losses
+    private String name;
+    private int wins;
+    private int losses;
+
+    // TODO: Define mutator methods -
+    // setName(), setWins(), setLosses()
+    public void setName(String userName) {
+        name = userName;
+    }
+
+    public void setWins(int userWins) {
+        wins = userWins;
+    }
+
+    public void setLosses(int userLosses) {
+        losses = userLosses;
+    }
+
+    // TODO: Define accessor methods -
+    // getName(), getWins(), getLosses()
+    public String getName() {
+        return name;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public int getLosses() {
+        return losses;
+    }
+
+    // TODO: Define getWinPercentage()
+    public double getWinPercentage() {
+        return wins / (double) (wins + losses);
+    }
+
+    // TODO: Define printStanding()
+    public void printStanding() {
+        System.out.print("Win percentage: ");
+        System.out.printf("%.2f", getWinPercentage());
+        System.out.println();
+        if (getWinPercentage() > 0.5) {
+            System.out.println("Congratulations, Team " + name + " has a winning average!");
+        } else {
+            System.out.println("Team " + name + " has a losing average.");
+        }
+    }
+
 }
--- lesson-9/TeamStats.java
+++ lesson-9/TeamStats.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
+
 public class TeamStats {
-    
+    @SuppressWarnings("resource")
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+
+        Team team = new Team();
+
+        String name = scnr.next();
+        int wins = scnr.nextInt();
+        int losses = scnr.nextInt();
+
+        team.setName(name);
+        team.setWins(wins);
+        team.setLosses(losses);
+
+        team.printStanding();
+    }
 }
