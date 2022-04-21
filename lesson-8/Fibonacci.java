afecf3ce692455ab630e8e43d03f53469705b409
Albert Ramos <albertramos.dev@gmail.com>
2022-04-21 11:20:04 -0700

    Add Fibonacci program

---
+++ lesson-8/Fibonacci.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+
+    public static int fibonacci(int n) {
+        if (n < 0)
+            return -1;
+        if (n == 0 || n == 1)
+            return n;
+
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    @SuppressWarnings("resource")
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int startNum;
+
+        startNum = scnr.nextInt();
+        System.out.println("fibonacci(" + startNum + ") is " + fibonacci(startNum));
+    }
+}
