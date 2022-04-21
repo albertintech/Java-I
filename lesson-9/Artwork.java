826ec26e83016a664c6139d575208eaef654ef87
Albert Ramos <albertramos.dev@gmail.com>
2022-04-20 21:29:13 -0700

    Lint file

--- lesson-9/Artwork.java
+++ lesson-9/Artwork.java
@@ -1,26 +1,30 @@
 public class Artwork {
 
-   private String title;
-   private int yearCreated;
-   private Artist artist;
+    private String title;
+    private int yearCreated;
+    private Artist artist;
 
-   public Artwork() {
-       title = "NoTitle";
-       yearCreated = 0;
-   }
-   public String getTitle(String userTitle) {
-       return title;
-   }
-   public int getYearCreated(int userYearCreated) {
-       return yearCreated;
-   }
-   public Artwork(String userTitle, int userYearCreated, Artist userArtist) {
-       title = userTitle;
-       yearCreated = userYearCreated;
-       artist = userArtist;
-   }
-   public printInfo() {
-       System.out.println("Artist: " + artist);
-       System.out.println("Title: " + title + ", " + yearCreated);
-   }                                                                                 
+    public Artwork() {
+        title = "NoTitle";
+        yearCreated = 0;
+    }
+
+    public String getTitle(String userTitle) {
+        return title;
+    }
+
+    public int getYearCreated(int userYearCreated) {
+        return yearCreated;
+    }
+
+    public Artwork(String userTitle, int userYearCreated, Artist userArtist) {
+        title = userTitle;
+        yearCreated = userYearCreated;
+        artist = userArtist;
+    }
+
+    public void printInfo() {
+        System.out.println("Artist: " + artist);
+        System.out.println("Title: " + title + ", " + yearCreated);
+    }
 }
