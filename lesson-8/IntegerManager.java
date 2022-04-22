f00e383b50a6642f23f0d3939b638827744162ad
Albert Ramos <albertramos.dev@gmail.com>
2022-04-22 08:43:26 -0700

    Add integer manager program

---
+++ lesson-8/IntegerManager.java
@@ -0,0 +1,30 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class IntegerManager {
+    public static void printSize(ArrayList<Integer> numsList) {
+        System.out.println("size " + numsList.size());
+    }
+
+    @SuppressWarnings("resource")
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int currVal;
+        ArrayList<Integer> intList = new ArrayList<Integer>();
+
+        printSize(intList);
+
+        currVal = scnr.nextInt();
+        while (currVal >= 0) {
+            intList.add(currVal);
+            currVal = scnr.nextInt();
+        }
+
+        printSize(intList);
+
+        intList.clear();
+
+        printSize(intList);
+    }
+}
