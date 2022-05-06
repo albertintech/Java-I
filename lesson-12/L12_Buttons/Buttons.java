8d9669edbeb7c4874ea097572428b0c79d9eba65
Albert Ramos <albertramos.dev@gmail.com>
2022-05-06 08:47:30 -0700

    Add name and date

--- lesson-12/L12_Buttons/Buttons.java
+++ lesson-12/L12_Buttons/Buttons.java
@@ -1,15 +1,14 @@
 /*
-   Name:  your name goes here
+   Name:  Albert Ramos
    Class: CIS163AA, Lesson 12 Lab #1
-   Date:  today's date
+   Date:  May 6, 2022
 */
 
 import javax.swing.JFrame;
 
-public class Buttons
-{
+public class Buttons {
 
-   public static void main(String[] args)	// Creates and displays the main program frame.
+   public static void main(String[] args) // Creates and displays the main program frame.
    {
       JFrame frame = new JFrame("Lesson 12 Lab 1 - Buttons");
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
