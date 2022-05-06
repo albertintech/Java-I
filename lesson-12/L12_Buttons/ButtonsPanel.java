8125e891ddcafb31bc1f9200f8bf06506af7203a
Albert Ramos <albertramos.dev@gmail.com>
2022-05-06 09:14:43 -0700

    Add solution

--- lesson-12/L12_Buttons/ButtonsPanel.java
+++ lesson-12/L12_Buttons/ButtonsPanel.java
@@ -7,25 +7,38 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.util.Random;
 
 public class ButtonsPanel extends JPanel {
    private JButton button1;
+   private JButton button2;
+   private JButton button3;
    private JLabel label;
    private JPanel buttonPanel;
+   private int count = 50;
+   private Random randGen = new Random();
 
    public ButtonsPanel() // Constructor: Sets up the GUI.
    {
       setLayout(new FlowLayout()); // Layout manager of choice: Flow
 
-      button1 = new JButton("Push Me");
+      button1 = new JButton("Increment");
       button1.addActionListener(new ButtonListener());
 
-      label = new JLabel("CIS163AA");
+      button2 = new JButton("Decrement");
+      button2.addActionListener(new ButtonListener());
+
+      button3 = new JButton("Randomize");
+      button3.addActionListener(new ButtonListener());
+
+      label = new JLabel("Count: " + count);
 
       buttonPanel = new JPanel();
       buttonPanel.setPreferredSize(new Dimension(600, 40));
-      buttonPanel.setBackground(Color.black);
+      buttonPanel.setBackground(Color.green);
       buttonPanel.add(button1);
+      buttonPanel.add(button2);
+      buttonPanel.add(button3);
 
       setPreferredSize(new Dimension(600, 80));
       setBackground(Color.white);
@@ -39,8 +52,16 @@ public class ButtonsPanel extends JPanel {
       public void actionPerformed(ActionEvent event) // Updates the counter and label when the button is pushed.
       {
          if (event.getSource() == button1) {
-            JOptionPane.showMessageDialog(null, "You clicked the button!", "Information",
-                  JOptionPane.INFORMATION_MESSAGE);
+            count++;
+            label.setText("Count: " + count);
+         }
+         if (event.getSource() == button2) {
+            count--;
+            label.setText("Count: " + count);
+         }
+         if (event.getSource() == button3) {
+            count = randGen.nextInt(100) + 1;
+            label.setText("Count: " + count);
          }
       }
    }
