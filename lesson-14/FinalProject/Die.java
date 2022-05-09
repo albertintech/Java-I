c03ca3e4dd321882b587547f15a4a082955e4dbd
Albert Ramos <albertramos.dev@gmail.com>
2022-05-09 09:42:24 -0700

    Update file structure

--- lesson-14/FinalProject/Die.java
+++ lesson-14/FinalProject/Die.java
@@ -2,18 +2,18 @@ package FinalProject;
 // package L14_FinalProject;
 
 public class Die {
-// TODO: declare variables
+    // TODO: declare variables
 
-// TODO: create constructor method
-// Initializes the die’s value to 0 and instantiates the Random object.
+    // TODO: create constructor method
+    // Initializes the die’s value to 0 and instantiates the Random object.
 
-// TODO: create roll() method
-// uses the Random object to simulate a die roll between 1 and 10.
+    // TODO: create roll() method
+    // uses the Random object to simulate a die roll between 1 and 10.
 
-// TODO: create equals(Die die2) method
-// returns Boolean true if both die have equal values, false otherwise.
+    // TODO: create equals(Die die2) method
+    // returns Boolean true if both die have equal values, false otherwise.
 
-// TODO: create toString() method
-// returns the String “one”, “two”, “three”, “four”, “five”, “six”, “seven”,
-// “eight”, “nine”, or “ten” depending on the value of the die.
+    // TODO: create toString() method
+    // returns the String “one”, “two”, “three”, “four”, “five”, “six”, “seven”,
+    // “eight”, “nine”, or “ten” depending on the value of the die.
 }
