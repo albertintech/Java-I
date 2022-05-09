821dfb2cdd3521e08b31497ba6b2e6b091c863b4
Albert Ramos <albertramos.dev@gmail.com>
2022-05-09 13:41:55 -0700

    Test switch case function: success

--- lesson-14/FinalProject/Die.java
+++ lesson-14/FinalProject/Die.java
@@ -1,19 +1,69 @@
 package FinalProject;
-// package L14_FinalProject;
+
+import java.security.SecureRandom;
 
 public class Die {
-    // TODO: declare variables
-
+    private int faceValue;
     // TODO: create constructor method
     // Initializes the die’s value to 0 and instantiates the Random object.
+    public Die() {
+        faceValue = 0;
+    }
+    public static int roll() {
+        int faceValue;
+        SecureRandom randGen = new SecureRandom();
 
-    // TODO: create roll() method
-    // uses the Random object to simulate a die roll between 1 and 10.
+        faceValue = randGen.nextInt(10) + 1;
+        return faceValue;
+    }
 
-    // TODO: create equals(Die die2) method
-    // returns Boolean true if both die have equal values, false otherwise.
-
+    public boolean equals(Die die2) {
+        if (faceValue != die2.faceValue) {
+            return false;
+        } else {
+            return true;
+        }
+    }
     // TODO: create toString() method
     // returns the String “one”, “two”, “three”, “four”, “five”, “six”, “seven”,
     // “eight”, “nine”, or “ten” depending on the value of the die.
+    public String toString(int x) {
+        String faceValueAsString;
+        switch (x) {
+            case 1: 
+                faceValueAsString = "one";
+                break;
+            case 2:
+                faceValueAsString = "two";
+                break;
+            case 3:
+                faceValueAsString = "three";
+                break;
+            case 4:
+                faceValueAsString = "four";
+                break;
+            case 5:
+                faceValueAsString = "five";
+                break;
+            case 6:
+                faceValueAsString = "six";
+                break;
+            case 7:
+                faceValueAsString = "seven";
+                break;
+            case 8:
+                faceValueAsString = "eight";
+                break;
+            case 9:
+                faceValueAsString = "nine";
+                break;
+            case 10:
+                faceValueAsString = "ten";
+                break;
+            default:
+                faceValueAsString = "Error";
+                break;
+        }
+        return faceValueAsString;
+    }
 }
--- lesson-14/FinalProject/DoubleDice.java
+++ lesson-14/FinalProject/DoubleDice.java
@@ -1,17 +1,26 @@
 package FinalProject;
 // package L14_FinalProject;
+import java.util.Scanner;
 
 public class DoubleDice {
+    @SuppressWarnings("resource")
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        double playerCash = 0.0;
+        double bet = 0.0;
+        int faceValue = 3;
 
-        // TODO: create main logic here
+        System.out.println("You have: " + playerCash);
+        Scanner scnr = new Scanner(System.in);
+        System.out.println("How much would you like to bet (Enter 0 to quit)? ");
+        bet = scnr.nextDouble();
+        System.out.println("You bet: " + bet);
+        //System.out.println("You rolled a " + die.roll() + " and " + die.roll());
+        System.out.println("You win/lose: " + playerCash);
+        Die myDie = new Die();
+        System.out.println("ToString test: " + myDie.toString(faceValue));
 
         // TODO: Reference your Die Class and corresponding methods for die
         // rolling.
-
-        // TODO: May either have statements all in a main() method or split
-        // logic up in separate methods.
 
     }
 }
