0ff3b9cc80683db2cb04db3e6342d8abf2574b09
Albert Ramos <albertramos.dev@gmail.com>
2022-05-09 16:34:24 -0700

    Add try/catch for input validation

--- lesson-14/FinalProject/DoubleDice.java
+++ lesson-14/FinalProject/DoubleDice.java
@@ -1,6 +1,8 @@
 package FinalProject;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.lang.Math;
 
 public class DoubleDice {
     @SuppressWarnings("resource")
@@ -18,10 +20,16 @@ public class DoubleDice {
             System.out.printf("You have: $%.2f\n", playerCash);
             Scanner scnr = new Scanner(System.in);
             System.out.print("How much would you like to bet (Enter 0 to quit)? ");
-            bet = scnr.nextDouble();
+            try {
+                bet = scnr.nextDouble();
+            } catch (InputMismatchException e) {
+                System.out.println("You must enter whole or decimal numbers only");
+                System.out.println("Goodbye!");
+                System.exit(0);
+            }
             if (bet == 0) {
                 break;
-            } else {
+            } else if (bet <= playerCash && bet > 0.0) {
                 System.out.printf("You bet: $%.2f\n", bet);
                 faceValue1 = myDie1.roll();
                 faceValue2 = myDie2.roll();
@@ -36,8 +44,18 @@ public class DoubleDice {
                     playerCash = playerCash - betLoss;
                     System.out.printf("You lose: $%.2f\n", bet);
                 }
+            } else {
+                System.out.println("You cannot bet less than zero or more than you have!");
             }
         }
-        System.out.println("Goodbye!");
+
+        if (playerCash > 200.0001) {
+            System.out.println("See you around, winner!");
+        } else if (Math.abs(playerCash) < 0.0001) {
+            System.out.println("You are out of money!");
+            System.out.println("Better luck next time!");
+        } else {
+            System.out.println("Goodbye!");
+        }
     }
 }
